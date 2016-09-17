e6dd42c77658328836a40591d25bbed5bbff65b0
Haub100 <haub100@mail.chapman.edu>
2016-09-17 14:46:40 -0700

    Character Controller
    Features Added:
    -FPS Character Controller (Unity)
    -Pause/Unpause on ESC
    -Mouse Lock/Unlock on ESC
    -Camera Freeze/Unfreeze on ESC
    -GameManager
    -MenuManager (Not sure about keeping this long term)
    
    Scenes Edited
    -Torch Placement

---
+++ UnderExplored/Assets/Assets/Code/Menus/Pause_Menu/Pause_Main.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Pause_Main : MonoBehaviour {
+
+	public GameObject GameManager;
+	//public Transform canvas;
+	public Transform Player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("escape") && GameManager.GetComponent<Mouse_Control>().getIsMouseLocked()){
+			GameManager.GetComponent<Mouse_Control>().setIsMouseLocked(false);
+			Time.timeScale = 0;
+			Player.GetComponent<RigidbodyFirstPersonController>().enabled = false;
+		} else if(Input.GetKeyDown("escape") && !GameManager.GetComponent<Mouse_Control>().getIsMouseLocked()){
+			GameManager.GetComponent<Mouse_Control>().setIsMouseLocked(true);
+			Time.timeScale = 1;
+			Player.GetComponent<RigidbodyFirstPersonController>().enabled = true;
+		}
+	}
+}
