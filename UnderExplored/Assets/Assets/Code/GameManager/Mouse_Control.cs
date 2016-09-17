12305348e54249d31afa52f343ce0601e9a1ea1b
Haub100 <haub100@mail.chapman.edu>
2016-09-17 14:52:48 -0700

    Changed Light_Source_Code
    Just changed the name and deleted the old one

---
+++ UnderExplored/Assets/Assets/Code/GameManager/Mouse_Control.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse_Control : MonoBehaviour {
+
+	private bool isMouseLocked;
+
+	// Use this for initialization
+	void Start () {
+		setIsMouseLocked(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Getters
+	public bool getIsMouseLocked(){
+		return isMouseLocked;
+	}
+
+	// Setters
+	public void setIsMouseLocked(bool isLocked){
+		isMouseLocked = isLocked;
+
+		if(isMouseLocked){
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		} else{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
+}
