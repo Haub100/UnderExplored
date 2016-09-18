581acf95198e47526f809122eb7dd1d9fd6a28ce
Haub100 <haub100@mail.chapman.edu>
2016-09-18 10:33:48 -0700

    Torch Angles
    Now when placing a torch on a surface it will match to the normals.

--- UnderExplored/Assets/Assets/Code/Character/Torch_Placement/centerCameraRaycast.cs
+++ UnderExplored/Assets/Assets/Code/Character/Torch_Placement/centerCameraRaycast.cs
@@ -7,6 +7,8 @@ public class centerCameraRaycast : MonoBehaviour
     public Camera mainCamera;
 	public GameObject torch;
 	public Vector3 torchSize;
+	public LayerMask mask = 0;
+	public float range;
 
     void Start()
     {
@@ -18,17 +20,40 @@ public class centerCameraRaycast : MonoBehaviour
         RaycastHit hit;
         if (Input.GetMouseButtonDown(0))
         {
-            if (Physics.Raycast(ray, out hit, 1f))
+            if (Physics.Raycast(ray, out hit, range, mask))
             {
                 print(hit.point);
 				print("normal: " + hit.normal);
                 print("I'm looking at " + hit.transform.name);
-				Quaternion rot = Quaternion.Euler(hit.normal.x, hit.normal.y, hit.normal.z);
-				GameObject placedTorch = Instantiate(torch, hit.point, rot) as GameObject;
+				//Quaternion rot = Quaternion.Euler(hit.normal.x, hit.normal.y, hit.normal.z);
+				GameObject placedTorch = Instantiate(torch, hit.point, Quaternion.identity) as GameObject;
 				placedTorch.transform.localScale = torchSize;
+				placedTorch.transform.rotation = Quaternion.FromToRotation (-transform.forward, hit.normal) * transform.rotation;
+
+				//RaycastHit[] RaycastHit = Physics.RaycastAll(ray, 2f);
+				/*Vector3 location = wallPlacementLocation(RaycastHit);
+				if(location == new Vector3(0f,0f,0f)){
+					GameObject placedTorch = Instantiate(torch, location, rot) as GameObject;
+					placedTorch.transform.localScale = torchSize;
+				}*/
+
             }
             else
                 print("I'm looking at nothing!");
         }
     }
+
+	private Vector3 wallPlacementLocation(RaycastHit[] hits){
+		Vector3 returnVec = new Vector3(0f,0f,0f);
+
+		foreach (RaycastHit hit in hits){
+			if(hit.transform.tag == "Wall"){
+				Debug.Log("Got here" + hit.point);
+				returnVec = hit.point;
+			}else{
+				continue;
+			}
+		}
+		return returnVec;
+	}
 }
