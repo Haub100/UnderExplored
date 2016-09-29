cebbc5f5e64eb9f022d07b851c59343fb5e92db0
Haub100 <haub100@mail.chapman.edu>
2016-09-29 11:56:42 -0700

    TorchUI & Torch Inventory
    Torch now exists so the player can place torches until 0 torches remain.
    There is also a maximum torch limit if player gain the ability to find
    more torches.

---
+++ UnderExplored/Assets/Assets/Code/Character/Inventory.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+	private int Torches;
+	private int TorchCapacity;
+	public GameObject TorchUIText;
+
+	// Use this for initialization
+	void Start () {
+		Torches = 5;
+		TorchCapacity = 5;
+		TorchUIText.GetComponent<Text>().text = Torches.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int getTorches(){
+		return Torches;
+	}
+
+	public int addTorches(int torchesAdded){
+		if(Torches < TorchCapacity){
+			Torches += torchesAdded;
+			if(Torches > TorchCapacity){
+				Torches = TorchCapacity;
+			}
+		}
+		else{
+			//torches is at maximum capacity
+		}
+		TorchUIText.GetComponent<Text>().text = Torches.ToString();
+		return Torches;		
+	}
+
+	public int removeTorches(int torchesRemoved){
+		if(Torches > 0){
+			Torches -= torchesRemoved;
+			if(Torches < 0){
+				Torches = 0;
+			}
+		}
+		else{
+			//torches already zero torches
+		}
+		TorchUIText.GetComponent<Text>().text = Torches.ToString();
+		return Torches;
+	}
+}
