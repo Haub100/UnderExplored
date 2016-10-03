b02e658bbc5e57f6648f73eb56157324e316be6c
Josh Smith <joshner.smith@gmail.com>
2016-10-02 22:44:16 -0700

    - Created a Node Manager
    - Node Manager Prefab Created
    - Node Manager object will keep track of any nodes you give it in the
    inspector
    - If all of these nodes all lit (isLit), then the nodemanager can send a
    message to other scripts to do whatever you'd like it to do. For
    instance, if you want a door to open when the conditions are met, but
    close when the torches are removed, just type in the methods you'd like
    to call in the NodeManger inspector window!
    - Added a Door control script to control a simple door opening/closing
    animation or position change

---
+++ UnderExplored/Assets/Assets/Code/Door Scripts/DoorScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+    public GameObject door; // The door object you would like to manipulate (through position or animation)
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Open a door door 
+    void OpenDoor()
+    {
+        print("Door Opening");
+    }
+
+    void CloseDoor()
+    {
+        print("Door Closing");
+    }
+}
--- UnderExplored/Assets/Assets/Code/Lighting_Detection_Scripts/NodeManager.cs
+++ UnderExplored/Assets/Assets/Code/Lighting_Detection_Scripts/NodeManager.cs
@@ -3,16 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 public class NodeManager : MonoBehaviour {
 
-    [SerializeField]
-    List<GameObject> nodes; // List of all torches
-	
-	// Use this for initialization
-	void Start () {
-	
+    // Be sure to drag in the nodes (which you are going to be detecting) into the Inspector window
+    
+    public List<GameObject> nodes; // List of all nodes
+    bool torchesNotCheckedYet; // Local boolean to make sure the update function doesn't keep spamming a lit boolean
+    public string methodToCallWhenLit; // What sort of action do you want to take once all of the nodes are lit?
+    public string methodToCallWhenNotLit; // What sort of action do you want to take once all of the nodes are NOT lit?
+
+    // Use this for initialization
+    void Start () {
+        torchesNotCheckedYet = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        // Do the below if all of your torches have successfully lit up all the nodes you care about
+        if (checkAllForLit() && torchesNotCheckedYet)
+        {
+            torchesNotCheckedYet = false;
+            SendMessage(methodToCallWhenLit);
+        }
+        //Do the below if the you no longer have all of the light powering your nodes anymore (Say, in case you want to close a door or deactivate a trap)
+        if (!checkAllForLit() && !torchesNotCheckedYet)
+        {
+            torchesNotCheckedYet = true;
+            SendMessage(methodToCallWhenNotLit);
+        }
+
+    }
+
+    // Go through the nodes one by one, making sure that all of them are activated (or lit up)
+    bool checkAllForLit()
+    {
+        foreach (GameObject node in nodes)
+        {
+            if (node.GetComponent<LightNode>().isLit)
+            {
+                //print("Node Meets Requirements");
+            }
+            else { return false; }
+        }
+        return true;
+    }
+
+   
 }
