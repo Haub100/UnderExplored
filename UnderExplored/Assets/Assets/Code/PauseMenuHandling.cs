96b8959b3f84e5d7d848d8f859122558f843b4c7
Josh Smith <joshner.smith@gmail.com>
2016-12-03 21:29:51 -0800

    - Working Pause menu on Level 1
    Go to prefabs/PauseMenu, and drag out both the Pause menu and the Menu
    handler objects into your scene. This will automatically make it work on
    any one of your scenes.

---
+++ UnderExplored/Assets/Assets/Code/PauseMenuHandling.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuHandling : MonoBehaviour {
+
+    public Texture cursorTexture;
+    public GameObject pauseMenu;
+    private PlayerHealth playerHealth;
+    
+    private bool isShowing;
+
+	// Use this for initialization
+	void Start () {
+        isShowing = false;
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        //Cursor.SetCursor(cursorTexture);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+            checkForMenu();
+    }
+
+    public void checkForMenu()
+    {
+        isShowing = !isShowing;
+        pauseMenu.SetActive(isShowing);
+        if (isShowing)
+        {
+            Cursor.visible = true;
+            Time.timeScale = 0.0f;
+            
+            
+        }
+            
+        if (!isShowing)
+        {
+            Cursor.visible = false;
+            Time.timeScale = 1.0f;
+            
+        }
+            
+        
+
+    }
+
+    public void resume()
+    {
+        checkForMenu();
+    }
+
+    public void respawn()
+    {
+        checkForMenu();
+        playerHealth.TakeDamage(100);
+    }
+
+    public void returnToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
