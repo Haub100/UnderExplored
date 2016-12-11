b1659d75a1991778edd6803e2f9c6df74ce9b93f
Haub100 <haub100@mail.chapman.edu>
2016-12-11 13:05:59 -0800

    Door Open/Close Audio

---
+++ UnderExplored/Assets/Assets/Code/Objects/doorFrameSound.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorFrameSound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip doorOpenSound;
+	[SerializeField]
+	private AudioClip doorCloseSound;
+    private AudioSource audioSource;
+    private bool soundPlayed;
+
+    // Use this for initialization
+    void Start()
+    {
+        soundPlayed = false;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<NodeController>().getIsLitEnough() && !soundPlayed)
+        {
+            soundPlayed = true;
+            audioSource.clip = doorOpenSound;
+			audioSource.Play();
+        }
+        else if (!this.GetComponent<NodeController>().getIsLitEnough() && soundPlayed)
+        {
+			audioSource.clip = doorCloseSound;
+			audioSource.Play();
+            soundPlayed = false;
+        }
+    }
+}
